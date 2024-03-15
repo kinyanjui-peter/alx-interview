9b30e5317c23200973ce64a4eb9351c154852b00
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-15 23:16:36 +0300

    lockboxex interviw question

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+def canUnlockAll(boxes):
+    if not boxes or len(boxes) == 0:
+        return False
+
+    keys = set([0])
+    # Start with the keys to the first box (index 0)
+    unlocked = set()
+
+    while keys:
+        box_idx = keys.pop()
+        # Get the index of the box with the key
+        unlocked.add(box_idx)
+        # Mark the box as unlocked
+
+        # Add all new keys found in the opened box
+        keys.update(boxes[box_idx])
+
+        # Remove keys to already opened boxes
+        keys.difference_update(unlocked)
+
+    return len(unlocked) == len(boxes)
