3c62eb00bbedf65e8e79f26e4be98717c1788e1a
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-03-22 16:27:02 +0300

    Pascals Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Function that prints Pascal's triangle.
+"""
+
+def pascal_triangle(n):
+    """Generate Pascal's triangle up to the nth row."""
+    if n <= 0 or not isinstance(n, int):
+        return []  # Return an empty list if n is non-positive or not an integer
+    
+    triangle = []
+    for row_num in range(n):
+        new_row = [1]
+        if row_num > 0:
+            prev_row = triangle[-1]
+            for y in range(1, row_num):
+                new_row.append(prev_row[y - 1] + prev_row[y])
+            new_row.append(1)
+        triangle.append(new_row)
+    
+    return triangle
+
+def print_triangle(triangle):
+    """Print Pascal's triangle."""
+    for row in triangle:
+        print(row)
