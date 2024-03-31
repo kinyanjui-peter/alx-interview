5be3fef7b2e101e34b6c19da17e6a7d2d909f980
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-01 01:14:36 +0300

    log passing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+A program that check userdata for a given format and return an output
 """
 import sys
 import datetime
@@ -7,11 +8,12 @@ import re
 
 
 def checkFormat(userInput):
-    """aA fuction that check validity of userInput data"""
+    """A fuction that check validity of userInput data"""
     pattern1 = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - '
-    pattern2 = r'\[\d{2}/(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/\d{4}\] '
+    pattern2 = r'\[\d{2}/(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/ '
+    pattern4 = r'\d{4}\] '
     pattern3 = r'"GET /projects/260 HTTP/1.1" \d{3} \d*$'
-    pattern = pattern1 + pattern2 + pattern3
+    pattern = pattern1 + pattern2 + pattern4 + pattern3
     if userInput == "":
         return ("Please input your Data")
     if not re.match(pattern, userInput):
@@ -26,6 +28,7 @@ def print_statistics(total_file_size, status_counts):
     for status_code, count in sorted(status_counts.items()):
         print(f"Status {status_code}: {count}")
 
+
 def main():
     total_file_size = 0
     status_counts = {}
@@ -34,22 +37,22 @@ def main():
     print("status code> <file size>")
 
     try:
-            for line in sys.stdin:
-                line = line.strip()
-                if checkFormat(line) == "Congratulations!!! Correct format":
-                    parts = line.split()
-                    file_size = int(parts[-1])
-                    total_file_size += file_size
-                    status_code = parts[-2]
-                    status_counts[status_code] = status_counts.get(status_code, 0) + 1
+        for line in sys.stdin:
+            line = line.strip()
+            if checkFormat(line) == "Congratulations!!! Correct format":
+                parts = line.split()
+                file_size = int(parts[-1])
+                total_file_size += file_size
+                status_code = parts[-2]
+                status_counts[status_code] = \
+                    status_counts.get(status_code, 0) + 1
 
-                if len(status_counts) >= 8:  # All possible status codes collected
-                    break
-
-        except KeyboardInterrupt:
-            pass
-
-        print_statistics(total_file_size, status_counts)
+            if len(status_counts) >= 8:
+                # All possible status codes collected
+                break
+    except KeyboardInterrupt:
+        pass
+    print_statistics(total_file_size, status_counts)
 
     if __name__ == "__main__":
         main()
