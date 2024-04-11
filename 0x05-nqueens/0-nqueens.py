c99cb698275445c4d9babcd5b2b805d4581af37e
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-11 22:34:35 +0300

    interview question

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,108 +1,74 @@
-#!/usr/bin/env python3
-"""
-N queens puzzle solution using backtracking algorithm
+#!/usr/bin/python3
+"""N Queens placement on an NxN chessboard.
+
+This script solves the N queens problem by placing N queens on an NxN chessboard
+such that no two queens threaten each other.
+
+Usage: nqueens N
+
+Arguments:
+    N: An integer representing the size of the chessboard and the number of queens.
+
+Constraints:
+    - N must be an integer greater than or equal to 4.
+
+The script prints all possible solutions to the problem.
 """
 
 import sys
 
 
-def is_safe(board, row, col, n):
-    """
-    Check if it's safe to place a queen at board[row][col]
-    """
-    # Check this row on left side
-    for i in range(col):
-        if board[row][i]:
-            return False
-
-    # Check upper diagonal on left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
-
-    # Check lower diagonal on left side
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
-
-    return True
+def generate_solutions(row, column):
+    solution = [[]]
+    for queen in range(row):
+        solution = place_queen(queen, column, solution)
+    return solution
 
 
-def solve_n_queens_util(board, col, n):
-    """
-    Utility function to solve N queens problem
-    """
-    # If all queens are placed, return true
-    if col >= n:
-        return True
-
-    # Consider this column and try placing this queen in all rows one by one
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            # Place this queen in board[i][col]
-            board[i][col] = 1
-
-            # Recur to place rest of the queens
-            if solve_n_queens_util(board, col + 1, n):
-                return True
-
-            # If placing queen in board[i][col] doesn't lead to a solution, then remove queen from board[i][col]
-            board[i][col] = 0
-
-    # If queen can't be placed in any row in this column, then return false
-    return False
+def place_queen(queen, column, prev_solution):
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
 
 
-def solve_n_queens(n):
-    """
-    Solve the N queens problem
-    """
-    # Initialize board
-    board = [[0 for _ in range(n)] for _ in range(n)]
-
-    # Start recursion from the first column
-    if not solve_n_queens_util(board, 0, n):
-        return False
-
-    return board
+def is_safe(q, x, array):
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
 
 
-def print_board(board):
-    """
-    Print the board
-    """
-    for row in board:
-        print(row)
-
-
-def main():
-    """
-    Main function
-    """
+def init():
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
-    try:
+    if sys.argv[1].isdigit():
         n = int(sys.argv[1])
-    except ValueError:
+    else:
         print("N must be a number")
         sys.exit(1)
-
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    # Solve N queens problem
-    board = solve_n_queens(n)
-
-    # Print solutions
-    if not board:
-        print("No solution found")
-    else:
-        for solution in board:
-            print(solution)
+    return (n)
 
 
-if __name__ == "__main__":
-    main()
+def n_queens():
+
+    n = init()
+    # generate all solutions
+    solutions = generate_solutions(n, n)
+    # print solutions
+    for array in solutions:
+        clean = []
+        for q, x in enumerate(array):
+            clean.append([q, x])
+        print(clean)
+
+
+if __name__ == '__main__':
+    n_queens()
