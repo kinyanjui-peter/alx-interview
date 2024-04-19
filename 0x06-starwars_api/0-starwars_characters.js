532cf428f22785755ac045f04c9e54c63f91fc54
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-04-19 05:54:54 +0300

    Star Wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+//interact with an external API to fetch and display information about
+//Star Wars characters based on the movie ID provided as an argument.
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
+    return;
+  }
+
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
+    }
+  });
+}
+
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
+});
