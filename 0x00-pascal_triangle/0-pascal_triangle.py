9fcedf9783556d39f11ed39aab3b5881acc59b96
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-05-04 00:12:27 +0300

    Pascal triagle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,28 +1,41 @@
 #!/usr/bin/python3
-"""
-Function that prints Pascal's triangle.
-"""
+"""A script to determine Pascal's triangle for any number of rows"""
 
 
 def pascal_triangle(n):
-    """Generate Pascal's triangle up to the nth row."""
-    if n <= 0 or not isinstance(n, int):
-        # Return an empty list if n is non-positive or not an integer
-        return []
+  """
+  Generate Pascal's triangle for a given number of rows (n).
 
-    triangle = []
-    for row_num in range(n):
-        new_row = [1]
-        if row_num > 0:
-            prev_row = triangle[-1]
-            for y in range(1, row_num):
-                new_row.append(prev_row[y - 1] + prev_row[y])
-            new_row.append(1)
-        triangle.append(new_row)
-    return triangle
+  Parameters:
+  n (int): Number of rows for Pascal's triangle.
+
+  Returns:
+  list of lists of integers: Pascal's triangle as a list of lists.
+                              Each inner list represents a row of the triangle.
+  """
+  triangle = []
+
+  if n <= 0:
+    return triangle  # Return empty triangle for non-positive n
+
+  for i in range(n):
+    temp_list = []
+
+    for j in range(i + 1):
+      if j == 0 or j == i:
+        temp_list.append(1)
+      else:
+        # Calculate element using values from the previous row
+        element = triangle[i - 1][j - 1] + triangle[i - 1][j]
+        temp_list.append(element)
+
+    triangle.append(temp_list)
+
+  return triangle
 
 
-def print_triangle(triangle):
-    """Print Pascal's triangle."""
-    for row in triangle:
-        print(row)
+# Example usage:
+if __name__ == "__main__":
+  num_rows = 5  # Specify the number of rows for Pascal's triangle
+  result = pascal_triangle(num_rows)
+  print(result)
