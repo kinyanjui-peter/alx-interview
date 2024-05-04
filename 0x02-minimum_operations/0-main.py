39f391c276af3183fdee02d3290087477b33d98c
kinyanjui-peter <01peterkinyanjui@gmail.com>
2024-05-04 19:00:04 +0300

    min operations

--- 0x02-minimum_operations/0-main.py
+++ 0x02-minimum_operations/0-main.py
@@ -10,14 +10,3 @@ print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
 
 n = 12
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))#!/usr/bin/python3
-"""
-Main file for testing
-"""
-
-minOperations = __import__('0-minoperations').minOperations
-
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
-n = 9
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,16 +3,19 @@
 """
 
 
-def minOperations(n):
-    if n <= 1:
-        return n
-    # Initialize an array to store the minimum number of operations
-    minNumber = [0] * (n + 1)
-    # Since we can't achieve 0 or 1 'H' characters, we start from 2
-    for i in range(2, n + 1):
-        minNumber[i] = float('inf')  # Initialize to infinity
-        # Try all possible factors of i to find the minimum operations
-        for j in range(1, i):
-            if i % j == 0:
-                minNumber[i] = min(minNumber[i], minNumber[j] + i // j)
-    return minNumber[n]
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
